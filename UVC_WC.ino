4162872999e41d2e87a45699a3f7bd0c3eee8b7d
Agustin Nunez <anunez@gmail.com>
2020-06-30 09:41:56 +0100

    v1.0 with debug messages

---
+++ UVC_WC.ino
@@ -0,0 +1,142 @@
+#include "ESP8266WiFi.h"
+#include "WiFiClient.h"
+
+#define RELAY D0         // RELAY = 1 uvc on, RELAY = 0 uvc off
+#define PIR   D1         // PIR = 1 presence, PIR = 0  NONE
+#define DOOR  D5         // DOOR = 1 closed, DOOR = 0 open 
+#define LED   D4         // LED = 0 light on, LED = 1 ligth off
+
+// global variables
+unsigned long ts1,ts2,ts3;
+unsigned long t1 = 5000;  // Presence delay while empty 5s
+unsigned long t2 = 10000;  // Presence delay while dirty 10s
+unsigned long t3 = 15000;  // Cleaning time 15s
+
+typedef enum state_t {
+    S_EMPTY_CLEAN,    // 0
+    S_SOMEONE_CLEAN,  // 1
+    S_BUSY, // 2
+    S_EMPTY_DIRTY, // 3
+    S_CLEANING // 4
+};
+
+static state_t state = S_EMPTY_DIRTY; // Initial state EMPTY_DIRTY
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  WiFi.mode( WIFI_OFF );
+  WiFi.forceSleepBegin();
+  pinMode(RELAY, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(DOOR, INPUT);
+  pinMode(PIR, INPUT);
+  uvc(LOW); 
+  Serial.begin(115200);
+  Serial.println(" ");
+  Serial.println("Inititialized");
+}
+
+void uvc(int uvc_status){
+  digitalWrite(RELAY, uvc_status);  
+  digitalWrite(LED, !uvc_status);  
+}
+
+
+void loop() {
+
+    switch (state)   {
+        case S_EMPTY_DIRTY:
+            if (digitalRead(DOOR)>0) {
+              Serial.println("DOOR CLOSED");
+              state = S_BUSY;
+              Serial.println("Change to state S_BUSY");
+              break;
+            }
+            if (digitalRead(PIR)>0) {
+              //Serial.println("PIR presence");
+              ts2 = millis();            
+              break;
+            }
+            if (millis() - ts2 > t2){
+               ts2=0;
+               Serial.println("T2_TIMEOUT");
+               ts3 = millis();            
+               Serial.println("RESET_T3");
+               state = S_CLEANING;
+               Serial.println("Change to state S_CLEANING");
+               uvc(HIGH);
+               Serial.println("RED LED WARNING: UVC CLEANING!");
+            }
+            break;
+        case S_CLEANING :
+            if (digitalRead(DOOR)>0) {
+              Serial.println("DOOR CLOSED");
+              uvc(LOW);
+              Serial.println("UVC CLEANING INTERRUPTED!");
+              state = S_BUSY;
+              Serial.println("Change to state S_BUSY");
+              break;
+            }
+            if (digitalRead(PIR)>0) {
+              Serial.println("PIR presence");
+              uvc(LOW);
+              Serial.println("UVC CLEANING INTERRUPTED!");
+              state = S_BUSY;
+              Serial.println("Change to state S_BUSY");
+              break;
+            }
+            if (millis() - ts3 > t3){
+               ts3=0;
+               Serial.println("T3_TIMEOUT");
+               ts3 = millis();            
+               Serial.println("RESET_T3");
+               state = S_EMPTY_CLEAN;
+               Serial.println("Change to state S_EMPTY_CLEAN");
+               Serial.println("GREEN LED");
+               digitalWrite(LED,1);
+            }
+            break;
+
+        case S_EMPTY_CLEAN:
+            if (digitalRead(PIR)>0) {
+              Serial.println("PIR presence");
+              state = S_SOMEONE_CLEAN;
+              Serial.println("Change to state S_SOMEONE_CLEAN");
+              ts1 = millis();            
+              Serial.println("RESET_T1");
+            }
+            break;
+
+        case S_SOMEONE_CLEAN:
+            if (millis() - ts1 > t1){
+               ts1=0;
+               Serial.println("T1_TIMEOUT");
+               state = S_SOMEONE_CLEAN;
+               Serial.println("Change to state S_SOMEONE_CLEAN");
+               break;
+            }
+            if (digitalRead(DOOR)>0) {
+              Serial.println("DOOR CLOSED");
+              state = S_BUSY;
+              Serial.println("Change to state S_BUSY");
+              Serial.println("RED LED");
+            }
+            break;
+
+        case S_BUSY:
+            if (digitalRead(DOOR)<1) {
+              Serial.println("DOOR OPEN");
+              state = S_EMPTY_DIRTY;
+              Serial.println("Change to state S_EMPTY_DIRTY");
+              ts2 = millis();         
+              Serial.println("RESET_T2");
+            }
+            break;
+
+        default:
+            Serial.print("Error state: ");
+            Serial.println(state);
+            break;
+    }
+}
